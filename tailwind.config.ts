e7cc530a254ce6fb0dbea1c2632890a2ddc12259
Murali P <fizzgamerowner@gmail.com>
2025-07-11 22:19:12 +0000

    use black hanny yellow etc that color look profasinal and good

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,7 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
+      padding: '2rem',
     },
     extend: {
       fontFamily: {
